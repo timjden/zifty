d47540bf93e694b9c1ca3d1c3921bd8174e46414
Timothy Dennis <timothy.dennis@hexagon.com>
2024-05-01 15:31:53 +0200

    get listing details if URL matches facebook listing

--- content.ts
+++ content.ts
@@ -1,8 +1,4 @@
-import type { PlasmoCSConfig } from "plasmo"
-
-export const config: PlasmoCSConfig = {
-  matches: ["https://www.facebook.com/marketplace/item/*"]
-}
+const URL_PATTERN = /https:\/\/www\.facebook\.com\/marketplace\/item\//
 
 function createOverlay() {
   const overlay = document.createElement("div")
@@ -61,13 +57,9 @@ function getLatLongFromStyle(xpath: string) {
   if (result.singleNodeValue) {
     const style = (result.singleNodeValue as HTMLElement).getAttribute("style")
     if (style) {
-      const backgroundImage = style.match(/background-image:url\((.*?)\)/)
-      if (backgroundImage) {
-        const url = backgroundImage[1]
-        const latLong = url.match(/center=(-?\d+\.\d+)%2C(-?\d+\.\d+)/)
-        if (latLong) {
-          return { lat: parseFloat(latLong[1]), long: parseFloat(latLong[2]) }
-        }
+      const latLong = style.match(/center=(-?\d+\.\d+)%2C(-?\d+\.\d+)/)
+      if (latLong) {
+        return { lat: parseFloat(latLong[1]), long: parseFloat(latLong[2]) }
       }
     }
   }
@@ -75,8 +67,11 @@ function getLatLongFromStyle(xpath: string) {
 }
 
 function getDetails(titleXpath: string, locationXpath: string) {
+  console.log("Getting details...")
   const title = getText(titleXpath)
+  console.log("Title: ", title)
   const location = getLatLongFromStyle(locationXpath)
+  console.log("Location: ", location)
   return {
     title,
     lat: location ? location.lat : null,
@@ -84,15 +79,42 @@ function getDetails(titleXpath: string, locationXpath: string) {
   }
 }
 
-window.addEventListener("load", () => {
+function onListingLoad() {
   const overlay = createOverlay()
   document.body.appendChild(overlay)
 
   const listingTitleXpath =
-    "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div[1]/h1/span"
-  const listingLocationXpath =
-    "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/div[1]/div[5]/div/div[2]/div[3]/div[1]/div/div/div/div[1]"
+    "//span[@class = 'x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x14z4hjw x3x7a5m xngnso2 x1qb5hxa x1xlr1w8 xzsf02u']"
+  const listingLocationXpath = "//*[contains(@style, 'MarketplaceStaticMap')]"
 
   const listingDetails = getDetails(listingTitleXpath, listingLocationXpath)
   console.log(listingDetails)
+  return listingDetails
+}
+
+// Check if the current URL is a Facebook Marketplace listing
+if (window.location.href.match(URL_PATTERN)) {
+  window.addEventListener("load", () => {
+    onListingLoad()
+  })
+}
+
+// Check if the URL is a Facebook Marketplace listing when the URL changes
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "URL changed") {
+    console.log("New URL: " + request.url)
+    if (request.url.match(URL_PATTERN)) {
+      // Call onListingLoad periodically until it meets the condition
+      let intervalId = setInterval(async () => {
+        let result = await onListingLoad()
+        if (!containsNullValues(result)) {
+          clearInterval(intervalId) // Stop calling when condition is met
+        }
+      }, 1000) // Check every 1 second
+    }
+  }
 })
+
+function containsNullValues(result) {
+  return !result || Object.values(result).some((x) => x === null)
+}
