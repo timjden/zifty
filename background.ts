e96c91d8aa6b2776fba725cecaa1805a2ba431be
Timothy Dennis <timothy.dennis@hexagon.com>
2024-05-01 17:15:25 +0200

    working fb query with hardcoded lat, lon, and title

--- background.ts
+++ background.ts
@@ -9,9 +9,112 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 })
 
 // Receive the listing details from content.ts
-chrome.runtime.onMessage.addListener((request) => {
+chrome.runtime.onMessage.addListener(async (request) => {
   if (request.type === "listingDetails") {
     console.log("Received result:", request.data)
+    const fbListings = await fetchFromFacebookMarketplace(
+      "bed",
+      { latitude: -33.9874, longitude: 18.4649 },
+      100
+    )
+    console.log(fbListings)
   }
   return true
 })
+
+async function fetchFromFacebookMarketplace(query, coordinates, radius) {
+  console.log(query)
+  console.log(coordinates)
+  console.log(radius)
+  console.log("Fetching from Facebook Marketplace")
+  try {
+    const url = "https://www.facebook.com/api/graphql/"
+
+    const headers = {
+      "sec-fetch-site": "same-origin",
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.62",
+      "content-type": "application/x-www-form-urlencoded"
+    }
+
+    const variables = {
+      buyLocation: {
+        latitude: coordinates.latitude,
+        longitude: coordinates.longitude
+      },
+      contextual_data: null,
+      count: 24,
+      cursor: null,
+      params: {
+        bqf: { callsite: "COMMERCE_MKTPLACE_WWW", query: query },
+        browse_request_params: {
+          commerce_enable_local_pickup: true,
+          commerce_enable_shipping: true,
+          commerce_search_and_rp_category_id: [],
+          commerce_search_and_rp_condition: null,
+          commerce_search_and_rp_ctime_days: null,
+          filter_location_latitude: coordinates.latitude,
+          filter_location_longitude: coordinates.longitude,
+          filter_price_lower_bound: 0,
+          filter_price_upper_bound: 214748364700,
+          filter_radius_km: radius
+        },
+        custom_request_params: {
+          browse_context: null,
+          contextual_filters: [],
+          referral_code: null,
+          saved_search_strid: null,
+          search_vertical: "C2C",
+          seo_url: null,
+          surface: "SEARCH",
+          virtual_contextual_filters: []
+        }
+      },
+      savedSearchID: null,
+      savedSearchQuery: query,
+      scale: 1,
+      shouldIncludePopularSearches: false,
+      topicPageParams: {
+        location_id: "capetown",
+        url: null
+      }
+    }
+
+    console.log(variables)
+
+    const payload = `variables=${encodeURIComponent(
+      JSON.stringify(variables)
+    )}&doc_id=7897349150275834&server_timestamps=true&fb_dtsg=`
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers,
+      body: payload,
+      credentials: "omit"
+    })
+    const data = await response.json()
+    console.log(data)
+    let listings = []
+
+    if (
+      !data.data?.marketplace_search?.feed_units?.edges[0]?.node?.listing?.id
+    ) {
+      listings = []
+    } else {
+      listings = data.data.marketplace_search.feed_units.edges.map((edge) => {
+        return {
+          id: edge.node.listing.id,
+          imageSrc: edge.node.listing.primary_listing_photo.image.uri,
+          link: `https://www.facebook.com/marketplace/item/${edge.node.listing.id}`,
+          title: edge.node.listing.marketplace_listing_title,
+          price: edge.node.listing.listing_price.formatted_amount,
+          location: edge.node.listing.location.reverse_geocode.city
+        }
+      })
+    }
+
+    return listings
+  } catch (error) {
+    console.error("Error fetching from Marketplace:", error)
+  }
+}
