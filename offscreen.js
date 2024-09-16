684e6fbed294675b0781778ab8893dcf696b84f9
Tim <timjden@gmail.com>
2024-09-16 16:35:11 +0200

    this now includes auth logic

--- offscreen.js
+++ offscreen.js
@@ -1,32 +1,77 @@
+// Geolocation and Auth combined offscreen.js
 chrome.runtime.onMessage.addListener(handleMessages);
+
+// The public URL for the authentication iframe
+const _URL = "https://zifty-4e74a.web.app/pages/sign-in-with-popup.html";
+console.log("Using URL:", _URL);
+const iframe = document.createElement("iframe");
+console.log("Iframe created:", iframe);
+iframe.src = _URL;
+console.log("Iframe source set to URL:", _URL);
+document.documentElement.appendChild(iframe);
+console.log("Iframe appended to document.");
+
+console.log("Iframe created and appended with URL:", _URL);
+
+// Main message handler
 function handleMessages(message, sender, sendResponse) {
+  console.log("Received message:", message);
+
   // Return early if this message isn't meant for the offscreen document.
   if (message.target !== "offscreen") {
-    return;
+    console.log("Message target is not 'offscreen'. Ignoring message.");
+    return false;
   }
 
-  if (message.type !== "get-geolocation") {
-    console.warn(`Unexpected message type received: '${message.type}'.`);
-    return;
+  switch (message.type) {
+    case "get-geolocation":
+      console.log("Handling geolocation request...");
+      handleGeolocationRequest(sendResponse);
+      break;
+    case "firebase-auth":
+      console.log("Handling Firebase auth request...");
+      handleAuthRequest(sendResponse);
+      break;
+    default:
+      console.warn(`Unexpected message type received: '${message.type}'.`);
+      return false;
   }
 
-  // You can directly respond to the message from the service worker with the
-  // provided `sendResponse()` callback. But in order to be able to send an async
-  // response, you need to explicitly return `true` in the onMessage handler
-  // As a result, you can't use async/await here. You'd implicitly return a Promise.
-  getLocation().then((loc) => sendResponse(loc));
-
+  // Returning true to indicate that the response will be sent asynchronously
   return true;
 }
 
-// getCurrentPosition() returns a prototype-based object, so the properties
-// end up being stripped off when sent to the service worker. To get
-// around this, create a deep clone.
+// Geolocation handling
+function handleGeolocationRequest(sendResponse) {
+  console.log("Starting geolocation retrieval...");
+  getLocation()
+    .then((loc) => {
+      console.log("Geolocation retrieved:", loc);
+      sendResponse(loc);
+    })
+    .catch((err) => {
+      console.error("Error retrieving geolocation:", err);
+      sendResponse({ error: err.message });
+    });
+}
+
+async function getLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (loc) => {
+        console.log("Geolocation success:", loc);
+        resolve(clone(loc));
+      },
+      (err) => {
+        console.error("Geolocation error:", err);
+        reject(err);
+      }
+    );
+  });
+}
+
 function clone(obj) {
   const copy = {};
-  // Return the value of any non true object (typeof(null) is "object") directly.
-  // null will throw an error if you try to for/in it. Just return
-  // the value early.
   if (obj === null || !(obj instanceof Object)) {
     return obj;
   } else {
@@ -37,14 +82,36 @@ function clone(obj) {
   return copy;
 }
 
-async function getLocation() {
-  // Use a raw Promise here so you can pass `resolve` and `reject` into the
-  // callbacks for getCurrentPosition().
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      (loc) => resolve(clone(loc)),
-      // in case the user doesnt have/is blocking `geolocation`
-      (err) => reject(err)
-    );
-  });
+// Firebase Auth handling
+function handleAuthRequest(sendResponse) {
+  console.log("Setting up message listener for iframe communication...");
+
+  globalThis.addEventListener("message", handleIframeMessage, false);
+
+  console.log("Sending 'initAuth' message to iframe...");
+
+  // Start the authentication flow in the iframe
+  iframe.contentWindow.postMessage({ initAuth: true }, new URL(_URL).origin);
+
+  function handleIframeMessage({ data }) {
+    console.log("Received message from iframe:", data);
+
+    try {
+      if (data.startsWith("!_{")) {
+        console.log("Ignoring non-auth related message from Firebase.");
+        return;
+      }
+      data = JSON.parse(data);
+      console.log("Parsed iframe data:", data);
+
+      // Remove the event listener after receiving the response
+      globalThis.removeEventListener("message", handleIframeMessage);
+      console.log("Iframe message listener removed.");
+
+      // Send the parsed data as a response
+      sendResponse(data);
+    } catch (e) {
+      console.error(`Failed to parse JSON from iframe message: ${e.message}`);
+    }
+  }
 }
