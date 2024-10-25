cd1bb54afb9b9fe036a948869ebf8a3c781805ae
Tim <timjden@gmail.com>
2024-10-25 09:49:47 +0200

    update popup based on toggle states

--- browser-extension/popup/popup.js
+++ browser-extension/popup/popup.js
@@ -1,3 +1,29 @@
+// Add event listeners to toggle switches
+document.querySelectorAll(".toggle").forEach((element) => {
+  element.addEventListener("change", handleToggleChange);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Get user settings i.e. the sites the user has toggled on/off
+  chrome.runtime.sendMessage({ message: "getToggleStates" }, (response) => {
+    console.log("Got toggle states:", response.data);
+    const toggleStates = response.data;
+    const toggles = document.querySelectorAll(".toggle");
+
+    // Loop through each toggle element and set its checked state based on stored values
+    toggles.forEach((toggle) => {
+      const toggleId = toggle.getAttribute("id"); // Assuming each toggle has an id attribute
+      if (toggleId && toggleStates.hasOwnProperty(toggleId)) {
+        toggle.checked = toggleStates[toggleId];
+      } else {
+        // If no stored state, set to false or default value
+        toggle.checked = false;
+      }
+    });
+  });
+});
+// });
+
 function handleToggleChange(event) {
   const isChecked = event.target.checked;
   const toggleId = event.target.id;
@@ -7,64 +33,6 @@ function handleToggleChange(event) {
     isChecked,
   });
 }
-
-// Add event listeners to toggle switches
-document.querySelectorAll(".toggle").forEach((element) => {
-  element.addEventListener("change", handleToggleChange);
-});
-
-function updateUI(response, handleSignIn, handleLogout, handleSubscribe) {
-  console.log("Updating UI");
-  // console.log(response);
-  // // Disable all toggle switches until UI is updated
-  // const toggleSwitches = document.querySelectorAll(".toggle");
-  // toggleSwitches.forEach((toggle) => {
-  //   toggle.disabled = true;
-  // });
-
-  // // Function to update the toggle states
-  // function updateToggleStates(toggleStatuses) {
-  //   // Map of toggle IDs to their corresponding statuses in the response
-  //   const toggleMap = {
-  //     amazon: toggleStatuses.amazon ?? true,
-  //     walmart: toggleStatuses.walmart ?? true,
-  //     takealot: toggleStatuses.takealot ?? true,
-  //     bol: toggleStatuses.bol ?? true,
-  //     temu: toggleStatuses.temu ?? true,
-  //     aliexpress: toggleStatuses.aliexpress ?? true,
-  //     google: toggleStatuses.google ?? true,
-  //     bing: toggleStatuses.bing ?? true,
-  //   };
-
-  //   // Iterate through each toggle and update its state
-  //   for (const [toggleId, isChecked] of Object.entries(toggleMap)) {
-  //     const toggleElement = document.getElementById(toggleId);
-  //     if (toggleElement) {
-  //       toggleElement.checked = isChecked;
-  //     } else {
-  //       console.error(`Toggle element with ID ${toggleId} not found.`);
-  //     }
-  //   }
-  // }
-
-  // // Enable all toggle switches after UI update
-  // toggleSwitches.forEach((toggle) => {
-  //   toggle.disabled = false;
-  // });
-  console.log("UI updated");
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM loaded");
-  const loadingDotsHTML =
-    '<span class="loading-dots"><span>.</span><span>.</span><span>.</span></span>';
-
-  // Proceed to get session details if the browser is supported
-  chrome.runtime.sendMessage({ message: "getSessionDetails" }, (response) => {
-    //updateUI(response, handleSignIn, handleLogout, handleSubscribe);
-  });
-});
-// });
 
 function formatDate(isoString) {
   const date = new Date(isoString);
